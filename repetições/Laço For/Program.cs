ee14b8435ffc0d833b14ffd80a11a4d857502d0c
Henrique Baptista De Oliveira <54454818835@info.local>
2023-04-06 14:21:07 -0300

    otherUpdate

---
+++ repetições/Laço For/Program.cs
@@ -0,0 +1,24 @@
+﻿// // Laço for = de 0 à 50
+
+// for (int x = 0; x <= 50; x++)
+// {
+//     Console.WriteLine(@$"{x}");
+    
+// }
+
+
+
+// // Laço for = de 200 à 100 (decrescente)
+
+// for (int y = 200; y >= 100; y -= 1)
+// {
+//     Console.WriteLine(@$"{y}");
+    
+// }
+
+
+// // Laço for, de 500 à 0 (decrescente em 5 em cinco)
+
+// for (int z = 500; z >= 0; z -= 5){
+//     Console.WriteLine(z);
+// }
