9803a1c5d9801b0835b30c074bcd85b707ea4bc7
Henrique Baptista De Oliveira <54454818835@info.local>
2023-04-10 16:56:22 -0300

    update3

---
+++ atividades/atividadeRepetições/atividadesEstruturasDeRepetição1/atividade5_idadePeso/Program.cs
@@ -0,0 +1,74 @@
+﻿// 5. Faça um programa que receba a idade, o peso e o sexo de 10 pessoas.
+// Calcule e imprima:
+
+// A.Total de Homens;      
+// B.Total de Mulheres;      
+// C.Média de idade dos Homens;      
+// D.Média de idade das mulheres.
+
+
+// algoritmo:
+
+// . 10 pessoas devem fazer a pesquisa
+// . peso, idade e sexo são variáveis
+// . peso = Float | idade = Int | sexo = Char
+// . char sexo deve aumentar a cada pessoa que participar da pesquisa,
+// tanto para M (sexo masculino) ou F (sexo feminino)
+// . imprimir a quantidade de mulheres e homens no final
+// . somar todas as idades de todas as mulheres e dividir pelo número
+// delas mesmas. A mesma coisa com os homens.
+
+int homens = 0;
+int mulheres = 0;
+
+int somaIdadeMulher = 0;
+int somaIdadeHomem = 0;
+
+
+int total = 0;
+for (total = 0; total < 4; total++){
+    Console.WriteLine(@$"
+    Digite seu sexo:
+    F - feminino | M - masculino
+    ");
+    bool sexo = false;
+    while (sexo == false) {
+        char genero = char.Parse(Console.ReadLine()!.ToLower());
+
+        switch(genero) {
+        case 'f':
+        mulheres++;
+        
+        Console.WriteLine($"Digite sua idade:");
+        int idadeF = int.Parse(Console.ReadLine()!);
+        somaIdadeMulher = somaIdadeMulher + idadeF;
+
+        Console.WriteLine($"Digite seu peso: ");
+        float pesoMulher = float.Parse(Console.ReadLine()!);
+
+        sexo = true;
+        total++;
+        break;
+
+        case 'm':
+        homens++;
+        
+        Console.WriteLine($"Digite sua idade:");
+        int idadeM = int.Parse(Console.ReadLine()!);
+        somaIdadeMulher = somaIdadeMulher + idadeM;
+
+        Console.WriteLine($"Digite seu peso: ");
+        float pesoHomens = float.Parse(Console.ReadLine()!);
+
+        sexo = true;
+        total++;
+        break;
+        
+        default:
+        Console.WriteLine($"Inválido");
+        break;
+        }
+
+}
+}
+Console.WriteLine($"Homens: {homens} | Mulheres: {mulheres}");
